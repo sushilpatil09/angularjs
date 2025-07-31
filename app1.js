ffce2d9aaa924abd7e99c74e25741140d6054091
sushil vijay patil <patilsushil991@gmail.com>
2025-07-31 15:19:12 +0530

    Delete app1.js

--- app1.js
+++
@@ -1,7 +0,0 @@
-angular.module('loginApp',[])
-.controller('logincontroller',function($scope) {
-    $scope.user = {};
-    $scope.login = function() {
-        alert("login successful!\nusername: " + $scope.user.username);
-  };
-});
